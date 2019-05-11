1c13384d7048a4e5e4ba11b4c3a4ea174d24092e
SrihariRamachandradas <sriharibr123@gmail.com>
2019-05-11 19:11:48 +0530

    The testcase for Drag and drop

--- tests/test_draganddropdemo.py
+++ tests/test_draganddropdemo.py
@@ -1,9 +1,3 @@
-import time
-
-from selenium import webdriver
-from testdata.data import *
-from pages.loginpage import LoginPage
-from pages.homepage import HomePage
 import pytest
 from pages.jqueryloginpage import Jquery_LoginPage
 
